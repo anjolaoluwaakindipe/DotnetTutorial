2e419335de04dd4952d59f644a2fa2adb3f23b5c
Anjolaoluwa Akindipe <anjyakindipe@gmail.com>
2021-12-31 03:57:02 -0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetTutorial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
